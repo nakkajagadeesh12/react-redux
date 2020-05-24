6099ead6d38710598fa4715d9a75b05ca39dda14
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2020-05-24 15:22:55 +0530

    1 file modified

--- Hello.js
+++ Hello.js
@@ -1,26 +1,43 @@
 import React from "react";
+import { createStore } from "redux";
 
 function Hello() {
+  const INC_VAL = "INC_VAL";
+
+  function incAction() {
+    return { type: INC_VAL, val: 33 };
+  }
+
+  const initialState = {
+    count: 0
+  };
+
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case INC_VAL:
+        return { ...state, count: state.count + action.val };
+      default:
+        return state;
+    }
+  };
+
+  const store = createStore(reducer);
+
+  console.log("getstate", store.getState());
+
+  const unsubscribe = store.subscribe(() =>
+    console.log("listener", store.getState())
+  );
+
+  store.dispatch(incAction());
+  store.dispatch(incAction());
+  store.dispatch(incAction());
+  store.dispatch(incAction());
+  store.dispatch(incAction());
+
+  unsubscribe();
+
   return <div>Redux</div>;
 }
 
 export default Hello;
-
-const INC_VAL = "INC_VAL";
-
-function incAction() {
-  return { type: INC_VAL, val: 33 };
-}
-
-const initialState = {
-  count: 0
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INC_VAL:
-      return { ...state, count: state.count + action.val };
-    default:
-      return state;
-  }
-};
