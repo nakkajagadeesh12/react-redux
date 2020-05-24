3111379be308f57f9587e210b88c8bbf3de46995
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2020-05-24 15:04:47 +0530

    1 file modified

--- Hello.js
+++ Hello.js
@@ -5,3 +5,22 @@ function Hello() {
 }
 
 export default Hello;
+
+const INC_VAL = "INC_VAL";
+
+function incAction() {
+  return { type: INC_VAL, val: 33 };
+}
+
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INC_VAL:
+      return { ...state, count: state.count + action.val };
+    default:
+      return state;
+  }
+};
